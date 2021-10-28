c2ca020d65113c4a475e194d4d06c26fa9710744
Mateusz <mateuszmikla@wp.pl>
2021-10-28 21:36:10 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Lista pacjentów</h1>
+    <div v-for="patient in patients" v-bind:key="patient.id">
+      <p class="patient">{{ patient.name }} {{ patient.lastName }}</p>
+      <div
+        class="medicine"
+        v-for="medicine in medicines"
+        v-bind:key="medicine.id"
+      >
+        {{
+          medicine.patientIds[0] === patient.id ? medicine.medicationName : null
+        }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      patients: [],
+      medicines: [],
+    };
+  },
+
+  methods: {
+    getPatients() {
+      axios
+        .get("https://cerber.pixel.com.pl/api/patients")
+        .then((response) => {
+          this.patients = response.data;
+          console.log(this.patients);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getMedicines() {
+      axios
+        .get("https://cerber.pixel.com.pl/api/medicine")
+        .then((response) => {
+          this.medicines = response.data;
+          console.log(this.medicines);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    filterPatients30() {},
+    filterPatients83() {},
+  },
+  beforeMount() {
+    this.getPatients();
+    this.getMedicines();
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.patient {
+  color: red;
 }
 </style>
